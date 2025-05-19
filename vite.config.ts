089bd5c80311676faa208351fb07b831d29073ca
Diogo Barreiras <diogobrsilva@gmail.com>
2025-05-19 15:28:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: "https://d-brsilva.github.io/pomodoro-todo-zen/",
   },
   plugins: [
     react(),
